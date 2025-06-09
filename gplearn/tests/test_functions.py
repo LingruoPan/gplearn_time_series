4363cd4fcb7b188548b5d9132920467e2761103a
Lingruo Pan <158210913+LingruoPan@users.noreply.github.com>
2025-06-09 13:54:42 +0800

    Update test_functions.py

--- gplearn/tests/test_functions.py
+++ gplearn/tests/test_functions.py
@@ -12,9 +12,9 @@ from sklearn.datasets import load_diabetes, load_breast_cancer
 from sklearn.utils._testing import assert_raises
 from sklearn.utils.validation import check_random_state
 
-from gplearn.functions import _protected_sqrt, make_function
-from gplearn.genetic import SymbolicRegressor, SymbolicTransformer
-from gplearn.genetic import SymbolicClassifier
+from .functions import _protected_sqrt, make_function
+from .genetic import SymbolicRegressor, SymbolicTransformer
+from .genetic import SymbolicClassifier
 
 # load the diabetes dataset and randomly permute it
 rng = check_random_state(0)
