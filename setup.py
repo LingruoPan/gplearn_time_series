392b48824d2e8f7365a1fc8b6b6638603dec17ee
Lingruo Pan <158210913+LingruoPan@users.noreply.github.com>
2025-06-09 14:17:13 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,20 @@
-#! /usr/bin/env python
-
-"""Genetic Programming in Python, with a scikit-learn inspired API"""
+# 推荐你这样改：
 
 from setuptools import setup, find_packages
-DESCRIPTION = __doc__
-VERSION  = '0.5.dev0' 
 
+DESCRIPTION = __doc__
+VERSION = '0.5.dev0'
 
 setup(name='gplearn_time_series',
       version=VERSION,
       description=DESCRIPTION,
       long_description=open("README.rst").read(),
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Intended Audience :: Science/Research',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved',
-                   'Topic :: Software Development',
-                   'Topic :: Scientific/Engineering',
-                   'Operating System :: Microsoft :: Windows',
-                   'Operating System :: Unix',
-                   'Operating System :: MacOS',
-                   'Programming Language :: Python',
-                   'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.8',
-                   'Programming Language :: Python :: 3.9',
-                   'Programming Language :: Python :: 3.10'],
+      classifiers=[...],
       author='Trevor Stephens',
       author_email='trev.stephens@gmail.com',
-      url='https://github.com/trevorstephens/gplearn',
+      url='https://github.com/LingruoPan/gplearn_time_series',
       license='new BSD',
       packages=find_packages(include=['gplearn_time_series', 'gplearn_time_series.*']),
-
       zip_safe=False,
       package_data={'': ['LICENSE']},
-      install_requires=['scikit-learn>=1.0.2',
-                        'joblib>=1.0.0'])
+      install_requires=['scikit-learn>=1.0.2', 'joblib>=1.0.0'])
